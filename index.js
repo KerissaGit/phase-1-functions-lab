4388c9fff65c08abf7b9af8e03fbe1f61790140c
KerissaGit <kerissa525@gmail.com>
2025-01-07 15:46:43 -0500

    13 Passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location){
+    return Math.abs(location - 42);
+}
+
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance > 2500){
+        return 'cannot travel that far';
+    }
+    if (distance <= 400){
+        return 0;
+    }
+    if (distance <= 2000){
+        return (distance - 400) * .02;
+    }
+    return 25;
+}
